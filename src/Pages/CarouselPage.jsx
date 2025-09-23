7b27b6b75bd5b7f9173d00b0b9bad3e3e8f658e2
NicooGon <nicolasd.gonzalez@correo.ucu.edu.uy>
2025-09-23 01:55:27 -0300

    form with emailjs

--- src/Pages/CarouselPage.jsx
+++ src/Pages/CarouselPage.jsx
@@ -1,40 +1,38 @@
 import CarouselImg from "@/Components/CarouselImg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function CarouselPage() {
-  
+
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
+
   return (
     <div
       id="quienes-somos"
       className="lg:min-h-[calc(90vh-80px)] bg-white flex flex-col items-center p-8 scroll-mt-54 sm:scroll-mt-54 md:scroll-mt-18 lg:scroll-mt-20"
     >
-      <div className="flex flex-col items-start w-full max-w-6xl"> 
-        <p 
-          className="font-medium text-gray-400 mb-2"
-          data-aos="fade-up"
-        >
-          Quienes somos
-        </p>
+      <div className="flex flex-col items-start w-full max-w-6xl" data-aos="fade-up"> 
+        <p className="font-medium text-gray-400 mb-2" data-aos="fade-up">Quienes somos</p>
         
-        <div 
-          className="flex flex-col md:flex-row justify-between items-start md:items-center w-full gap-6"
-          data-aos="fade-up"
-          data-aos-delay="100"
-        >
-          <p className="text-3xl sm:text-4xl max-w-md sm:max-w-lg lg:max-w-xl leading-snug text-gray-900 font-montserrat">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full gap-6" data-aos="fade-up" data-aos-delay="100">
+          <p className="text-3xl sm:text-4xl max-w-md sm:max-w-lg lg:max-w-xl leading-snug text-gray-900 font-montserrat"  data-aos="fade-up" data-aos-delay="200">
             Nos especializamos en reparación y mantenimiento de estructuras de difícil acceso.
           </p>
 
-          <div className="flex flex-col gap-10">
-            <div className="relative group" data-aos="fade-up" data-aos-delay="200">
+          <div className="flex flex-col gap-10" data-aos="fade-up" data-aos-delay="300">
+            <div className="relative group" data-aos="fade-up" data-aos-delay="400">
               <p className="cursor-pointer">
                 edpinturasgaggioni@gmail.com
               </p>
-              <div className="absolute bottom-0 left-0 h-0.5 w-full bg-gray-200 rounded-full overflow-hidden">
+              <div className="absolute bottom-0 left-0 h-0.5 w-full bg-gray-200 rounded-full overflow-hidden" >
                 <div className="h-0.5 w-0 bg-gradient-to-r from-red-600 via-red-400 to-red-600 transition-all duration-500 ease-out group-hover:w-full rounded-full"></div>
               </div>
             </div>
 
-            <a href="#contactanos" data-aos="fade-up" data-aos-delay="300">
+            <a href="#contactanos" data-aos="fade-up" data-aos-delay="500">
               <button className="w-64 px-6 py-3 bg-gray-100 hover:bg-gray-200 text-black font-semibold font-inter rounded-md transition">
                 ¡Hace tu consulta!
               </button>
@@ -43,11 +41,9 @@ export default function CarouselPage() {
           </div>
         </div>
 
-        <div className="w-28 h-0.5 bg-gradient-to-r from-red-600 via-red-400 to-red-600 rounded-full mt-6 mb-16" data-aos="fade-up" data-aos-delay="400"></div>
+        <div className="w-28 h-0.5 bg-gradient-to-r from-red-600 via-red-400 to-red-600 rounded-full mt-6 mb-16" data-aos="fade-up" data-aos-delay="600"></div>
         
-        <div data-aos="fade-up" data-aos-delay="500">
-          <CarouselImg />
-        </div>
+        <CarouselImg />
       </div>
     </div>
   );
--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import image from "../Images/img4.jpeg";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import emailjs from "@emailjs/browser"; 
 
 export default function ContactPage() {
   const [nombre, setNombre] = useState("");
@@ -11,6 +12,36 @@ export default function ContactPage() {
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
   }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+     setNombre("");
+     setEmail("");
+     setMensaje("");
+
+    const templateParams = {
+      nombre,
+      email,
+      mensaje,
+    };
+
+    emailjs
+      .send(
+        "service_co4mf2f",  
+        "template_qo1ym8b",  
+        templateParams,
+        "_ZNjolpch2zZwbvXL"    
+      )
+      .then(
+        (response) => {
+          alert("Correo enviado con éxito!");
+        },
+        (error) => {
+          alert("Error al enviar el correo, intente nuevamente.");
+          console.error(error);
+        }
+      );
+  };
 
   return (
     <div
@@ -24,7 +55,7 @@ export default function ContactPage() {
       >
         <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg" data-aos="fade-up" data-aos-delay="100">
           <h2 className="text-3xl font-bold mb-6 text-center">Pinturas Gaggioni</h2>
-          <form className="flex flex-col space-y-4" onSubmit={(e) => e.preventDefault()}>
+          <form className="flex flex-col space-y-4" onSubmit={sendEmail}>
             <div className="flex flex-col">
               <label className="text-gray-700 font-medium mb-1">Nombre *</label>
               <input
@@ -60,14 +91,12 @@ export default function ContactPage() {
               />
             </div>
 
-            <a
-              href={`https://mail.google.com/mail/?view=cm&fs=1&to=edpinturasgaggioni@gmail.com&su=Hola, soy ${encodeURIComponent(nombre)}&body=${encodeURIComponent(mensaje)}`}
+            <button
+              type="submit"
               className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 rounded text-center transition"
-              target="_blank"
-              rel="noopener noreferrer"
             >
               Enviar
-            </a>
+            </button>
           </form>
         </div>
 
@@ -89,9 +118,7 @@ export default function ContactPage() {
             </a>
           </div>
           <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=edpinturasgaggioni@gmail.com&su=Consulta&body=Hola,%20quiero%20información"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="mailto:edpinturasgaggioni@gmail.com"
             className="text-gray-800 hover:text-white"
           >
             ✉️ Gmail: edpinturasgaggioni@gmail.com
